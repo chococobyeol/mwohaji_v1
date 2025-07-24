d83b2749ddef41039be8c23f4fafe967497d783d
BuildTools <unconfigured@null.spigotmc.org>
2025-07-24 22:33:14 +0900

    반복 아이콘 수정

--- js/icons.js
+++ js/icons.js
@@ -20,7 +20,7 @@ const icons = (() => {
         clock: '<circle cx="12" cy="12" r="10"/><polyline points="12,6 12,12 16,14"/>', // 시계
         bell: '<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="m13.73 21a2 2 0 0 1-3.46 0"/>', // 알림 설정
         bellOff: '<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="m13.73 21a2 2 0 0 1-3.46 0"/><line x1="4" y1="4" x2="20" y2="20"/>', // 알림 끄기
-        repeat: '<polyline points="17,1 21,5 17,9"/><path d="M21 5H9a4 4 0 0 0-4 4v6"/><polyline points="7,23 3,19 7,15"/><path d="M3 19h12a4 4 0 0 0 4-4V9"/>', // 반복
+        repeat: '<path d="M17 2l4 4-4 4"/><path d="M21 6H9a4 4 0 0 0-4 4v2"/><path d="M7 22l-4-4 4-4"/><path d="M3 18h12a4 4 0 0 0 4-4v-2"/>', // 반복 (순환 화살표)
         
         // 데이터 관리
         upload: '<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7,10 12,5 17,10"/><line x1="12" y1="5" x2="12" y2="15"/>', // 가져오기 (↑)
